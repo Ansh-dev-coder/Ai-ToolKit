73ab19b44ba2518d0f28c8efd9ea61c901021ce9
Ansh <saxenaansh27@gmail.com>
2026-07-06 15:01:33 +0530

    Initial Project Setup

---
+++ src/main/java/com/ai/AiToolkit/AiToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.ai.AiToolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiToolkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/AiToolkit/AiToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.AiToolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
