d8f21793ca0b1d92bf641a96c9ff44c5044abd4e
Ansh <saxenaansh27@gmail.com>
2026-07-06 17:42:50 +0530

    Add getResponseOptions method to ChatService for enhanced response handling

--- src/main/java/com/ai/AiToolkit/Service/ChatService.java
+++ src/main/java/com/ai/AiToolkit/Service/ChatService.java
@@ -1,6 +1,9 @@
 package com.ai.AiToolkit.Service;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +16,22 @@ public class ChatService
     public ChatService(ChatModel chatModel){
         this.chatModel=chatModel;
     }
-//Method to get the response from the chat model
+    //Method to get the response from the chat model
     public String getResponse(String prompt){
-       return chatModel.call(prompt);
+        return chatModel.call(prompt);
+    }
+//Method to get the response from the chat model
+    public String getResponseOptions(String prompt){
+       ChatResponse response= chatModel.call(new Prompt(
+               prompt,
+               GoogleGenAiChatOptions.builder()
+                       .model("gemini-2.5-flash")
+                       .googleSearchRetrieval(true)
+//                       .includeServerSideToolInvocations(true)
+                       .build()
+
+       ));
+       return  response.getResult().getOutput().getText();
     }
     
 }
